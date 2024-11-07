cf91f5bfa00d2747018189ca01181920f8e262b1
Sam Taylor <s.taylor2099@outlook.com>
2024-11-07 18:23:00 +0000

    updated auth-app-stack as tables moved

--- lib/auth-app-stack.ts
+++ lib/auth-app-stack.ts
@@ -7,47 +7,17 @@ import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as custom from "aws-cdk-lib/custom-resources";
 import { games, gameCompanies } from "../seed/games";
 import { generateBatch } from "../shared/util";
+import * as lambdanode from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apig from "aws-cdk-lib/aws-apigateway";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class AuthAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // GameCompanies table
-    const gameCompaniesTable = new dynamodb.Table(this, "GameCompaniesTable", {
-      partitionKey: { name: "companyId", type: dynamodb.AttributeType.NUMBER },
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      tableName: "GameCompanies",
-    });
 
-    // Games table
-    const gamesTable = new dynamodb.Table(this, "GamesTable", {
-      partitionKey: { name: "companyId", type: dynamodb.AttributeType.NUMBER },
-      sortKey: { name: "gameId", type: dynamodb.AttributeType.NUMBER },
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      tableName: "Games",
-    });
-
-    // MARSHALLING DATA FOR TABLES
-    new custom.AwsCustomResource(this, "gamesTableInitData", {
-      onCreate: {
-        service: "DynamoDB",
-        action: "batchWriteItem",
-        parameters: {
-          RequestItems: {
-            [gamesTable.tableName]: generateBatch(games),
-            [gameCompaniesTable.tableName]: generateBatch(gameCompanies),
-          },
-        },
-        physicalResourceId: custom.PhysicalResourceId.of(
-          "gamesTableInitData"
-        ),
-      },
-      policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
-        resources: [gamesTable.tableArn, gameCompaniesTable.tableArn],
-      }),
-    });
 
     // Cognito User Pool for authentication
     const userPool = new UserPool(this, "UserPool", {
@@ -71,7 +41,6 @@ export class AuthAppStack extends cdk.Stack {
     new AppApi(this, "AppApi", {
       userPoolId: userPoolId,
       userPoolClientId: userPoolClientId,
-      gamesTable: gamesTable,
-      gameCompanyTable: gameCompaniesTable,
+  
     });
   }}
